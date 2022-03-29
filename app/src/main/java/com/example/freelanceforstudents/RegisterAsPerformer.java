f6871941783c336742f77cea8b9fc1d077a94a33
Ekaterina Zimanova <51347241+LuiOz1996@users.noreply.github.com>
2022-03-29 16:55:47 +0300

    FFS

---
+++ app/src/main/java/com/example/freelanceforstudents/RegisterAsPerformer.java
@@ -0,0 +1,14 @@
+package com.example.freelanceforstudents;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RegisterAsPerformer extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register_as_performer);
+    }
+}
